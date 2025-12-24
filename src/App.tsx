a730824947a2ef3eb417ddf96b3935518afe579e
Binyam Tagel <binyam.tagel@gmail.com>
2025-12-24 10:08:55 +0300

    fix: correct import path for VideosPage and adjust div class order

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { HomePage } from "./pages/Home";
-import { VideosPage } from "./pages/videos";
+import { VideosPage } from "./pages/Videos";
 import { BlogPage } from "@/pages/Blog";
 
 export default function App() {
@@ -25,7 +25,7 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <Header currentPage={currentPage} onNavigate={handleNavigate} />
       <main className="flex-1">{renderPage()}</main>
       <Footer />
